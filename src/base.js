72acdabf0745e732c1d1b372032e3954d725ae56
Heather B <heatherbrokmeier@gmail.com>
2017-06-21 18:01:39 -0500

    first commit

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyAa_usgYAT8VI8rPvT-BhDRIR4M-4s0Je8",
+    authDomain: "catch-of-the-day-b5bad.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-b5bad.firebaseio.com",
+});
+
+export default base;
